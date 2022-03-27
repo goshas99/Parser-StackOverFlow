58544b6bc77309f3856b171b70496e1ca38906e6
Georgy Spiridonov <gosha583@gmail.com>
2022-03-27 12:15:10 +0300

    Update

---
+++ Parser.py
@@ -0,0 +1,18 @@
+from bs4 import BeautifulSoup
+import lxml
+import requests
+
+
+def main():
+    base = 'https://ru.stackoverflow.com'
+    html = requests.get(base).content
+    soup = BeautifulSoup(html, 'lxml')
+    div = soup.find('div', id='question-mini-list')
+    a = div.find_all('a', class_="s-link")
+
+    for _ in a:
+        print(_.getText(), base + _.get('href'))
+
+
+if __name__ == "__main__":
+    main()
